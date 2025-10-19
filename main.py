4c0f636788450907d0c31c2df282f78ea8902f59
bronzeorb83 <bronzeorb83@gmail.com>
2025-10-19 21:14:05 +0200

    added first word count and stats.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import count_words
+
 def get_book_text(path):
 
     with open(path) as f:
@@ -6,6 +8,8 @@ def get_book_text(path):
 
 def main():
     text = get_book_text("books/frankenstein.txt")
-    print(text)
+    #print(text)
+    num_words = count_words(text)
+    print(f"Found {num_words} total words")
 
 main()
---
+++ stats.py
@@ -0,0 +1,7 @@
+def count_words(text):
+
+    count = 0
+    words = text.split()
+    for word in words:
+        count +=1
+    return count
